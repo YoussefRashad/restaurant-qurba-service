2d8a25fcc75ba0398caea4041b98d20977d2716c
YoussefRashad <youssefrashad119@gmail.com>
2022-03-06 04:44:36 +0200

    add message file

---
+++ src/config/Messages.ts
@@ -0,0 +1,12 @@
+export default {
+  restaurant: {
+    error: {
+      NOT_FOUND: "Restaurant does not exist",
+    },
+  },
+  user: {
+    error: {
+      NOT_AUTHORIZED: "you are not authorized to do this operation",
+    },
+  },
+};
--- src/controller/restaurant.controller.ts
+++ src/controller/restaurant.controller.ts
@@ -3,6 +3,7 @@ import { Response } from "express";
 import HttpStatusCodes from "http-status-codes";
 import Restaurant from "../models/restaurant.model";
 import { IRestaurant } from "../models/restaurant.model";
+import Messages from "../config/Messages";
 
 export const fetchRestaurant = async (req: Request, res: Response) => {
   const restaurants = await Restaurant.find();
@@ -10,9 +11,9 @@ export const fetchRestaurant = async (req: Request, res: Response) => {
 };
 export const getRestaurant = async (req: Request, res: Response) => {
   const { code }: { code: string } = req.body;
-  const restaurant = await get(code);
+  const restaurant = await getRestaurantByCode(code);
   if (!restaurant) {
-    return res.status(HttpStatusCodes.BAD_REQUEST).send();
+    return res.status(HttpStatusCodes.BAD_REQUEST).send(Messages.restaurant.error.NOT_FOUND);
   }
   return res.status(HttpStatusCodes.OK).send(restaurant);
 };
@@ -31,15 +32,19 @@ export const searchRestaurant = async (req: Request, res: Response) => {
     },
   ]);
   if (!restaurants.length) {
-    return res.status(HttpStatusCodes.BAD_REQUEST).send();
+    return res.status(HttpStatusCodes.BAD_REQUEST).send(Messages.restaurant.error.NOT_FOUND);
   }
   return res.status(HttpStatusCodes.OK).send(restaurants);
 };
 
 export const createRestaurant = async (req: Request, res: Response) => {
-  const restaurant = new Restaurant({ ...req.body });
-  await restaurant.save();
-  return res.status(HttpStatusCodes.CREATED).send(restaurant);
+  try {
+    const restaurant = new Restaurant({ ...req.body });
+    await restaurant.save();
+    return res.status(HttpStatusCodes.CREATED).send(restaurant);
+  } catch (error) {
+    return res.status(HttpStatusCodes.BAD_REQUEST).send(error.message);
+  }
 };
 export const editRestaurant = async (req: Request, res: Response) => {
   const {
@@ -47,9 +52,9 @@ export const editRestaurant = async (req: Request, res: Response) => {
     user_id,
     ...rest
   }: { code: string; user_id: string; rest: IRestaurant } = req.body;
-  const restaurant = await get(code);
+  const restaurant = await getRestaurantByCode(code);
   if (!restaurant) {
-    return res.status(HttpStatusCodes.BAD_REQUEST).send();
+    return res.status(HttpStatusCodes.BAD_REQUEST).send(Messages.restaurant.error.NOT_FOUND);
   }
   checkIfAuthorized(user_id, restaurant);
   await restaurant.updateOne(rest);
@@ -58,21 +63,21 @@ export const editRestaurant = async (req: Request, res: Response) => {
 
 export const deleteRestaurant = async (req: Request, res: Response) => {
   const { code, user_id }: { code: string; user_id: string } = req.body;
-  const restaurant = await get(code);
+  const restaurant = await getRestaurantByCode(code);
   if (!restaurant) {
-    return res.status(HttpStatusCodes.BAD_REQUEST).send();
+    return res.status(HttpStatusCodes.BAD_REQUEST).send(Messages.restaurant.error.NOT_FOUND);
   }
   checkIfAuthorized(user_id, restaurant);
   await restaurant.delete();
   return res.status(HttpStatusCodes.OK).send(restaurant);
 };
 
-const get = async (code: string) => {
+const getRestaurantByCode = async (code: string) => {
   return await Restaurant.findOne({ code });
 };
 
 const checkIfAuthorized = (user_id: string, restaurant: IRestaurant) => {
   if (+user_id === +restaurant.user_id) {
-    throw new Error("restaurant does not exist");
+    throw new Error(Messages.user.error.NOT_AUTHORIZED);
   }
 };
